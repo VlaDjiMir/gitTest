ea95293ecdad8827c68bef12b3c0e20169ec3b44
VlaDjiMir <ammazvv@gmail.com>
2019-02-19 19:28:35 +0900

    g append

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 let svg = d3.selectAll('body').append('svg')
 	.attr('width', 500)
 	.attr('heigth', 340);
+
+function appendG(x, y) {
+	g.append('g')
+		.data([{ "x": x, "y": y }])
+		.attr('transform', 'translate(' + x + ',' + y + ')');
+	
+	g.append('circle')
+		.attr('r', 5)
+		.attr('fill', 'orange');
+}
+
+appendG(100, 100);
