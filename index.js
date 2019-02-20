a3b47656d16af92494b18ffc8a65d918a52395eb
VlaDjiMir <ammazvv@gmail.com>
2019-02-20 18:30:39 +0900

    Добавление группы

--- index.js
+++ index.js
@@ -2,14 +2,37 @@ let svg = d3.selectAll('body').append('svg')
 	.attr('width', 500)
 	.attr('height', 340);
 
-function appendG(x, y) {
-	let g = svg.append('g')
-		.data([{ "x": x, "y": y }])
-		.attr('transform', 'translate(' + x + ',' + y + ')');
-	
-	g.append('circle')
-		.attr('r', 5)
-		.attr('fill', 'orange');
+let data_1 = [
+	{"x": 100, "y": 100},
+	{"x": 100, "y": 150},
+	{"x": 100, "y": 200},
+	{"x": 100, "y": 50},
+	{"x": 100, "y": 250}
+];
+
+let data_2 = [
+	{"x": 200, "y": 100},
+	{"x": 200, "y": 150},
+	{"x": 200, "y": 200}
+]
+
+function appendG(data) {
+	let g = svg.append('g');
+	let ex = [];
+
+	for (let i = 0; i < data.length; i++) {
+		g.append('circle')
+			.attr('cx', data[i].x)
+			.attr('cy', data[i].y)
+			.attr('r', 5)
+			.attr('fill', 'orange');
+
+		ex.push(data[i].y);
+	}
+
+	g.append('path')
+		.attr('d', 'M' + data[0].x + ',' + d3.min(ex) + 'V' + d3.max(ex))
+		.attr('stroke', 'orange');
 }
 
-appendG(100, 100);
+appendG(data_1);
