31a8d7bd9c238c970f31055859d6a504a2bd592f
VlaDjiMir <ammazvv@gmail.com>
2019-02-22 17:48:28 +0900

    Добавлен scaleTime функция

--- index.js
+++ index.js
@@ -20,16 +20,29 @@ let data_2 = [
 ];
 
 let data_3 = [
-	{"x": 200, "y": 100},
-	{"x": 200, "y": 150},
-	{"x": 200, "y": 200}
+	{"x": 250, "y": 90},
+	{"x": 250, "y": 120},
+	{"x": 250, "y": 150}
 ];
+
+let data_4 = {
+	"position": 300,
+	"event": [
+		{"id": 1, "date": "01-01-2000"},
+		{"id": 2, "date": "02-02-2000"},
+		{"id": 3, "date": "03-03-2000"},
+		{"id": 4, "date": "04-04-2000"}
+	]
+}
+
+let formatTime = d3.timeFormat("%d-%m-%Y");
+let parseTime = d3.timeParse("%d-%m-%Y");
 
 let xScale = d3.scaleLinear()
 	.domain([0, w/2])
 	.range([0, w]);
 let yScale = d3.scaleTime()
-	.domain([new Date(2016, 0, 1), new Date(2019, 2, 20)])
+	.domain([new Date(1999, 10, 1), new Date(2000, 6, 1)])
 	.range([0, h]);
 
 let xAxis = d3.axisTop()
@@ -63,5 +76,31 @@ function appendG(data) {
 		.attr('stroke', 'orange');
 }
 
+function appendT(data) {
+	let ex = [];
+
+	let g = svg.append('g');
+
+	for (let i = 0; i < data.event.length; i++) {
+		data.event[i].date = parseTime(data.event[i].date);
+		
+		g.append('circle')
+			.attr('cx', data.position)
+			.attr('cy', yScale(data.event[i].date))
+			.attr('r', 3)
+			.attr('fill', 'orange');
+
+		ex.push(data.event[i].date);
+	}
+
+	console.log(ex);
+
+	g.append('path')
+		.attr('d', 'M' + data.position + ',' + yScale(d3.min(ex)) + 'V' + yScale(d3.max(ex)))
+		.attr('stroke', 'orange');
+}
+
 appendG(data_1);
 appendG(data_2);
+appendG(data_3);
+appendT(data_4);
