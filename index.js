2ed7064faba08a378cbb23c613116eb1f3fe2e4a
VlaDjiMir <ammazvv@gmail.com>
2019-02-22 22:35:04 +0900

    Удален appendG

--- index.js
+++ index.js
@@ -5,44 +5,23 @@ let svg = d3.selectAll('body').append('svg')
 	.attr('width', w)
 	.attr('height', h);
 
-let data_1 = [
-	{"x": 100, "y": 100},
-	{"x": 100, "y": 150},
-	{"x": 100, "y": 200},
-	{"x": 100, "y": 50},
-	{"x": 100, "y": 250}
-];
-
-let data_2 = [
-	{"x": 200, "y": 100},
-	{"x": 200, "y": 150},
-	{"x": 200, "y": 200}
-];
-
-let data_3 = [
-	{"x": 250, "y": 90},
-	{"x": 250, "y": 120},
-	{"x": 250, "y": 150}
-];
-
 let data_4 = {
 	"position": 300,
 	"event": [
-		{"id": 1, "date": "01-01-2000"},
-		{"id": 2, "date": "02-02-2000"},
-		{"id": 3, "date": "03-03-2000"},
-		{"id": 4, "date": "04-04-2000"}
+		{"id": 1, "date": "01-01-2010"},
+		{"id": 2, "date": "02-02-2011"},
+		{"id": 3, "date": "02-02-2012"}
 	]
 }
 
-let formatTime = d3.timeFormat("%d-%m-%Y");
-let parseTime = d3.timeParse("%d-%m-%Y");
+let formatTime = d3.timeFormat("%d-%m-%Y"); // Перевести из машинной даты в стройчный
+let parseTime = d3.timeParse("%d-%m-%Y"); // Перевести строчной даты в машинный
 
 let xScale = d3.scaleLinear()
 	.domain([0, w/2])
 	.range([0, w]);
 let yScale = d3.scaleTime()
-	.domain([new Date(1999, 11, 15), new Date(2000, 4, 4)])
+	.domain([new Date(2009,1,1), new Date(2013,2,2)])
 	.range([0, h]);
 
 let xAxis = d3.axisTop()
@@ -50,60 +29,33 @@ let xAxis = d3.axisTop()
 let yAxis = d3.axisRight()
 	.scale(yScale);
 
-let gX = svg.append('g').call(xAxis)
+let gX = svg.append('g').call(xAxis) // Создать ось X
 	.attr('transform', 'translate(0,' + (h+2) + ')');
-let gY = svg.append('g').call(yAxis)
+let gY = svg.append('g').call(yAxis) // Создать ось Y
 	.attr('transform', 'translate(-2, 0)');
-
-
-function appendG(data) {
-	let ex = [];
-	
-	let g = svg.append('g');
-
-	for (let i = 0; i < data.length; i++) {
-		g.append('circle')
-			.attr('cx', data[i].x)
-			.attr('cy', data[i].y)
-			.attr('r', 3)
-			.attr('fill', 'orange');
-
-		ex.push(data[i].y);
-	}
-
-	g.append('path')
-		.attr('d', 'M' + data[0].x + ',' + d3.min(ex) + 'V' + d3.max(ex))
-		.attr('stroke', 'orange');
-}
 
 function appendT(data) {
 	let ex = [];
-
-	let g = svg.append('g');
-
+	let g = svg.append('g'); // Создать группу для процесса
 	for (let i = 0; i < data.event.length; i++) {
-		data.event[i].date = parseTime(data.event[i].date);
-		
-		g.append('circle')
+		data.event[i].date = parseTime(data.event[i].date); // Парсинг даты в машинный
+		g.append('circle') // Отрисовка кругов
 			.attr('cx', data.position)
 			.attr('cy', yScale(data.event[i].date))
 			.attr('r', 3)
 			.attr('fill', 'orange')
 			.attr('id', data.event.id);
-
-		ex.push(data.event[i].date);
+		ex.push(data.event[i].date); // Добавить Y координату в массив
 	}
-
-	// Задать домэйн для yAxis
-	console.log(d3.min(ex));
-	console.log(d3.max(ex));
-
-	g.append('path')
+	g.append('path') // Создать path путем нахождения min и max значений
 		.attr('d', 'M' + data.position + ',' + yScale(d3.min(ex)) + 'V' + yScale(d3.max(ex)))
 		.attr('stroke', 'orange');
+
+	yScale.domain([new Date(1980,1,1), new Date(2013,2,2)]);	
+
+	let min = d3.min(ex);
+	let max = d3.max(ex);
+	console.log(min + " -------- " + max);
 }
 
-appendG(data_1);
-appendG(data_2);
-appendG(data_3);
 appendT(data_4);
