3df59be42649b568077640178805561285f8ca91
VlaDjiMir <ammazvv@gmail.com>
2021-02-11 23:15:18 +0900

    add script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+const promise = () => {
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            console.info('MY PROMISE')
+            res()
+        }, 2000)
+    })
+}
+
+promise().then(() => {
+    console.info('YOUR PROMISE')
+})
